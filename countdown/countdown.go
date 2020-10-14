840f592862d30711fa62302b2d8196ecb4877376
jasrana <694074954@qq.com>
2020-10-14 09:50:57 +0800

    增加监视器监视睡眠次数

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -10,16 +10,39 @@ import (
 const countdownStart = 3
 const finalWord = "Go!"
 
+type Sleeper interface {
+	Sleep()
+}
+
+// mock spies 监视器，监视执行的次数
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+}
+
+func (o *ConfigurableSleeper) Sleep() {
+	time.Sleep(o.duration)
+}
+
 // 依赖注入，运行时打印到命令行，测试时输出到 buffer
-func Countdown(writer io.Writer)  {
+// 问题引入：每次需要花费 4s 的时间执行 Countdown 测试
+func Countdown(writer io.Writer, sleeper Sleeper)  {
 	for i := countdownStart; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 		fmt.Fprintln(writer, i)
 	}
-	time.Sleep(1 * time.Second)
+	sleeper.Sleep()
 	fmt.Fprint(writer, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &ConfigurableSleeper{1 * time.Second}
+	Countdown(os.Stdout, sleeper)
 }
