69248c608e751f2eb9627d5da3aab4a4fbea7a9d
jasrana <694074954@qq.com>
2020-10-14 09:33:48 +0800

    输出完整结果

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -6,9 +6,15 @@ import (
 	"os"
 )
 
+const countdownStart = 3
+const finalWord = "Go!"
+
 // 依赖注入，运行时打印到命令行，测试时输出到 buffer
 func Countdown(writer io.Writer)  {
-	fmt.Fprint(writer, 3)
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(writer, i)
+	}
+	fmt.Fprint(writer, finalWord)
 }
 
 func main() {
