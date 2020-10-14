ff0f32ef0baa443442773c49590357badcdfcef2
jasrana <694074954@qq.com>
2020-10-14 09:34:57 +0800

    每隔1s执行一次

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const countdownStart = 3
@@ -12,8 +13,10 @@ const finalWord = "Go!"
 // 依赖注入，运行时打印到命令行，测试时输出到 buffer
 func Countdown(writer io.Writer)  {
 	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(writer, i)
 	}
+	time.Sleep(1 * time.Second)
 	fmt.Fprint(writer, finalWord)
 }
 
