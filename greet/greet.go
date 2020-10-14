3ba7b9910d4c0d203d1e94d88d65d4f13ff79564
jasrana <694074954@qq.com>
2020-10-14 09:16:45 +0800

    使用浏览器，重用 Greet()

--- greet/greet.go
+++ greet/greet.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"net/http"
 )
 
+// 依赖注入的原理
+// https://studygolang.gitbook.io/learn-go-with-tests/go-ji-chu/dependency-injection
+// 想要重用，注入自己的 io.Writer 即可
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+// go run greet.go
+// localhost:5000
 func main() {
-	Greet(os.Stdout, "Elodie")
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
 }
