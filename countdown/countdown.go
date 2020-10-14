14f63e8ab773699f98a26363aa260a0c52f468bc
jasrana <694074954@qq.com>
2020-10-14 10:18:44 +0800

    实现对执行顺序的测试

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -31,8 +31,29 @@ func (o *ConfigurableSleeper) Sleep() {
 	time.Sleep(o.duration)
 }
 
+// 记录操作次数与操作类型
+type CountdownOperationSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+// 实现了 io.writer
+func (s *CountdownOperationSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const sleep = "sleep"
+const write = "write"
+
 // 依赖注入，运行时打印到命令行，测试时输出到 buffer
 // 问题引入：每次需要花费 4s 的时间执行 Countdown 测试
+// 问题引入：代码的执行顺序无法简单使用 SpySleeper 监视
+// mock 的原则
+// https://studygolang.gitbook.io/learn-go-with-tests/go-ji-chu/mocking
 func Countdown(writer io.Writer, sleeper Sleeper)  {
 	for i := countdownStart; i > 0; i-- {
 		sleeper.Sleep()
