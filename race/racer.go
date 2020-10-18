b18eb05400c2bbb0af5a3d74e4773245178ee7d7
jasrana <694074954@qq.com>
2020-10-18 14:10:58 +0800

    使用 select，并发发送请求

--- race/racer.go
+++ race/racer.go
@@ -1,22 +1,24 @@
 package race
 
-import (
-	"net/http"
-	"time"
-)
+import "net/http"
 
+// 问题引入：无法同时测试，浪费时间
 func Racer(urlA, urlB string) (winner string) {
-	durationUrlA := measureResponseTime(urlA)
-	durationUrlB := measureResponseTime(urlB)
-
-	if durationUrlA < durationUrlB {
+	// 谁先返回，谁就胜出
+	// select 允许同时在多个 channel 等待
+	select {
+	case <-ping(urlA):
 		return urlA
+	case <-ping(urlB):
+		return urlB
 	}
-	return urlB
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan bool{
+	ch := make(chan bool)
+	go func() {
+		http.Get(url)
+		ch <- true
+	}()
+	return ch
 }
