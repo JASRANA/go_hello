fab06fcf252b4f2ca81196bd5889eb86eb40f9e5
jasrana <694074954@qq.com>
2020-10-18 13:04:42 +0800

    使用 chanel 解决竞争条件

--- concurrency/checkwebsites.go
+++ concurrency/checkwebsites.go
@@ -1,23 +1,38 @@
 package concurrency
 
-import "time"
+import "fmt"
 
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChanel := make(chan result)
 
 	for _, url := range urls {
 		// 通过将 go 放在 func() 前，开启 goroutine
 		// 匿名函数
 		// 如果不将 url 分别传入，那么都会使用同一个 url 的引用
 		// 并发写入 Map 将报错！使用 go test -race 发现 *竞争条件*
+		// 每个进程使用同一个通道 channel，每次将结果放进通道中
 		go func(u string) {
-			results[u] = wc(u)
+			resultChanel <- result{u, wc(u)}
 		}(url)
 	}
 
-	time.Sleep(2 * time.Second)
+	fmt.Println("after")
+	// 排队将结果输出到 results
+	for i := 0; i < len(urls); i++ {
+		result := <- resultChanel
+		fmt.Println(result.string)
+		results[result.string] = result.bool
+	}
+
+	// 不需要再进行休眠等待
+	// time.Sleep(2 * time.Second)
 
 	return results
 }
