b1daa1bdeb2c8383e2b52055117a608fc27d2f02
jasrana <694074954@qq.com>
2020-10-18 13:39:34 +0800

    使用 mock http 服务器做测试

--- race/racer_test.go
+++ race/racer_test.go
@@ -1,15 +1,34 @@
 package race
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURl := "http://www.github.com"
-	fastURL := "http://www.baidu.com"
+	// 制作测试用 HTTP 服务器，通过控制请求返回时间达到测试目的
+	slowServer := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		// 先休眠 20 毫秒
+		time.Sleep(20 * time.Millisecond)
+		writer.WriteHeader(http.StatusOK)
+	}))
 
-	got := Racer(slowURl, fastURL)
+	fastServer := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	got := Racer(slowURL, fastURL)
 	want := fastURL
 
 	if got != want {
 		t.Errorf("got '%s' want '%s'", got, want)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 }
