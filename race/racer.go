a5fa183ace92b1366dff95862a5d4ac3a4b38e76
jasrana <694074954@qq.com>
2020-10-19 10:00:05 +0800

    超过 10s，报错

--- race/racer.go
+++ race/racer.go
@@ -1,16 +1,23 @@
 package race
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
 // 问题引入：无法同时测试，浪费时间
-func Racer(urlA, urlB string) (winner string) {
+func Racer(urlA, urlB string) (winner string, err error) {
 	// 谁先返回，谁就胜出
 	// select 允许同时在多个 channel 等待
 	select {
 	case <-ping(urlA):
-		return urlA
+		return urlA, nil
 	case <-ping(urlB):
-		return urlB
+		return urlB, nil
+		// time.After() 会在定义的时间过后发送一个 chan
+	case <-time.After(10 * time.Second):
+		return "", fmt.Errorf("timed out waiting for %s and %s", urlA, urlB)
 	}
 }
 
--- race/racer_test.go
+++ race/racer_test.go
@@ -7,33 +7,48 @@ import (
 	"time"
 )
 
-const slowDelayTime = 20
-const fastDelayTime = 0
-
 func TestRacer(t *testing.T) {
-	slowServer := makeDelayedServer(slowDelayTime)
-	fastServer := makeDelayedServer(fastDelayTime)
+	//t.Run("测试谁先胜出", func(t *testing.T) {
+	//	slowServer := makeDelayedServer(10 * time.Second)
+	//	fastServer := makeDelayedServer(0)
+	//
+	//	// 包裹 defer 的函数结束时调用后边的语句
+	//	// https://studygolang.gitbook.io/learn-go-with-tests/go-ji-chu/select#defer
+	//	defer slowServer.Close()
+	//	defer fastServer.Close()
+	//
+	//	got, _ := Racer(slowURL, fastURL)
+	//	want := fastURL
+	//
+	//	slowServer.Close()
+	//	fastServer.Close()
+	//
+	//	if got != want {
+	//		t.Errorf("got '%s' want '%s'", got, want)
+	//	}
+	//})
 
-	// 包裹 defer 的函数结束时调用后边的语句
-	// https://studygolang.gitbook.io/learn-go-with-tests/go-ji-chu/select#defer
-	defer slowServer.Close()
-	defer fastServer.Close()
+	t.Run("超过 10s，报错", func(t *testing.T) {
+		slowServer := makeDelayedServer(11 * time.Second)
+		fastServer := makeDelayedServer(12 * time.Second)
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	got := Racer(slowURL, fastURL)
-	want := fastURL
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
-	}
+		_, err := Racer(slowURL, fastURL)
 
+		if err == nil {
+			t.Fatal("expected an error but got nil.")
+		}
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		time.Sleep(delay * time.Millisecond)
+		time.Sleep(delay)
 		writer.WriteHeader(http.StatusOK)
 	}))
 }
